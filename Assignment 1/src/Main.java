12072c0eeb332f3003913b858b388f1d99a81729
JesLaz <jelazo@live.com>
2020-10-01 15:19:47 -0700

    Set up assignment one

---
+++ Assignment 1/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void partOne(){
+        String first = "fqjcb rwjwj vnjax bnkhj whxcq naqjv nfxdu mbvnu ujbbf nnc";
+        String second = "oczmz vmzor jocdi bnojv dhvod igdaz admno ojbzo rcvot jprvi oviyv\n" +
+                "aozmo cvooj ziejt dojig toczr dnzno jahvi fdiyv xcdzq zoczn zxjiy";
+        String third = "ejitp spawa qleji taiul rtwll rflrl laoat wsqqj atgac kthls iraoa\n" +
+                "twlpl qjatw jufrh lhuts qataq itats aittk stqfj cae";
+        String fourth = "iyhqz ewqin azqej shayz niqbe aheum hnmnj jaqii yuexq ayqkn jbeuq\n" +
+                "iihed yzhni ifnun sayiz yudhe sqshu qesqa iluym qkque aqaqm oejjs\n" +
+                "hqzyu jdzqa diesh niznj jayzy uiqhq vayzq shsnj jejjz nshna hnmyt\n" +
+                "isnae sqfun dqzew qiead zevqi zhnjq shqze udqai jrmtq uishq ifnun\n" +
+                "siiqa suoij qqfni syyle iszhn bhmei squih nimnx hsead shqmr udquq\n" +
+                "uaqeu iisqe jshnj oihyy snaxs hqihe lsilu ymhni tyz ";
+    }
+
+    public static String decrypt(String cipher, int key){
+        String returnValue;
+        for (int i = 0; i < cipher.length(); i++){
+            char temp = cipher.charAt(i);
+            if(Character.isLowerCase(temp)){
+                char wait;
+            }
+        }
+    }
+
+
+    public static void main(String args[]){
+        partOne();
+    }
+}
