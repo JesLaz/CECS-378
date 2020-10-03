e0157beb451617ffb9d4478aea161ab38ae9b3b3
JesLaz <jelazo@live.com>
2020-10-02 23:01:27 -0700

    Implement simple-substitution cipher via encryption and decryption functions

--- Assignment 1/src/Main.java
+++ Assignment 1/src/Main.java
@@ -1,31 +1,93 @@
-import java.util.Scanner;
+import java.nio.charset.Charset;
+import java.util.*;
+
 public class Main {
-    public static void partOne(){
-        String first = "fqjcb rwjwj vnjax bnkhj whxcq naqjv nfxdu mbvnu ujbbf nnc";
-        String second = "oczmz vmzor jocdi bnojv dhvod igdaz admno ojbzo rcvot jprvi oviyv\n" +
-                "aozmo cvooj ziejt dojig toczr dnzno jahvi fdiyv xcdzq zoczn zxjiy";
-        String third = "ejitp spawa qleji taiul rtwll rflrl laoat wsqqj atgac kthls iraoa\n" +
-                "twlpl qjatw jufrh lhuts qataq itats aittk stqfj cae";
-        String fourth = "iyhqz ewqin azqej shayz niqbe aheum hnmnj jaqii yuexq ayqkn jbeuq\n" +
-                "iihed yzhni ifnun sayiz yudhe sqshu qesqa iluym qkque aqaqm oejjs\n" +
-                "hqzyu jdzqa diesh niznj jayzy uiqhq vayzq shsnj jejjz nshna hnmyt\n" +
-                "isnae sqfun dqzew qiead zevqi zhnjq shqze udqai jrmtq uishq ifnun\n" +
-                "siiqa suoij qqfni syyle iszhn bhmei squih nimnx hsead shqmr udquq\n" +
-                "uaqeu iisqe jshnj oihyy snaxs hqihe lsilu ymhni tyz ";
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    /**
+     * Returns a modified version of the lowercase alphabet by randomly scrambling a string of the alphabet
+     * @return String, for use as a key
+     */
+    public static String[] createKey(){
+        String key = "abcdefghijklmnopqrstuvwxyz";
+        String[] alphaCopy = key.split("");//easier than manually filling out the array
+        List<String> alphaList = Arrays.asList(alphaCopy);
+        Collections.shuffle(alphaList);
+        alphaList.toArray(alphaCopy);
+        return(alphaCopy);
     }
 
-    public static String decrypt(String cipher, int key){
-        String returnValue;
-        for (int i = 0; i < cipher.length(); i++){
-            char temp = cipher.charAt(i);
-            if(Character.isLowerCase(temp)){
-                char wait;
+    /**
+     * Former half of the simple-substition cipher process which operates via replacing each character by another
+     * one character relative to a key, assumes the given text is lowercase
+     * @param plaintext the phrase entered to be encrypted into a cipher text
+     * @param key the certain modified version of the alphabet for the encryption to base of, same key to be used in
+     *            decryption
+     * @return String, the encrypted message of the inputted phrase
+     */
+    public static String myEncrypt(String plaintext, String[] key){
+        String cipher = "";
+        Map<Character, String> keyMap = new HashMap<Character, String>();
+        //Iterate through the alphabet and key simultaneously to create a map of the two in which the plaintext
+        //can substitute letters of the original alphabet to the modified alphabet aka the key
+        for(int i = 0; i < alphabet.length(); i++){
+            keyMap.put(alphabet.charAt(i), key[i]);
+        }
+        System.out.println("KeyMap formatted as follows: ");
+        System.out.println(keyMap);
+        for(int i = 0; i < plaintext.length(); i++){
+            //to retain formatting only perform substitution to alphabetical characters
+            if(Character.isAlphabetic(plaintext.charAt(i))){
+                char temp = plaintext.charAt(i);
+                cipher = cipher + keyMap.get(temp);
+            }
+            else{
+                cipher += plaintext.charAt(i);
             }
         }
+        return cipher;
+    }
+
+    /**
+     * Latter half of the simple-substition cipher process, reverse substitution is used by comparing the ciphertext
+     * to the key to return the original text, ciphertext should remain lowercase
+     * @param cipher the desired ciphertext to be decrypted accordingly with the proper key
+     * @param key the certain modified version of the alphabet for the decryption to utilize, same key as was used in
+     *            the encryption process
+     * @return String, the plaintext message which the ciphertext is based off
+     */
+    public static String decrypt(String cipher, String[] key){
+        String pText = "";
+        Map<Character, Character> keyMap = new HashMap<Character, Character>();
+        //Similar creation of a map as in the encryption process however as the desired value desired is the original
+        //alphabet, the key and values of the map are switched
+        for(int i = 0; i < alphabet.length(); i++){
+            keyMap.put((key[i].charAt(0)), alphabet.charAt(i));
+        }
+        for(int i = 0; i < cipher.length(); i++){
+            //for formatting retention
+            if(Character.isAlphabetic(cipher.charAt(i))){
+                pText = pText + keyMap.get(cipher.charAt(i));
+            }
+            else{
+                pText += cipher.charAt(i);
+            }
+        }
+        return pText;
     }
 
 
     public static void main(String args[]){
-        partOne();
+        String[] key = createKey();
+        System.out.print("Randomly generated key: ");
+        for(String c : key){
+            System.out.print(c);
+        }
+        System.out.println("");
+        String cipher = myEncrypt("hello there", key);
+        String decode = decrypt(cipher, key);
+
+        System.out.println("Ciphertext: " + cipher);
+        System.out.println("Plaintext: " + decode);
     }
 }
